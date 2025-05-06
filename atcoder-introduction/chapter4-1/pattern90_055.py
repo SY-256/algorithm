be41db5de15b0ed6364795b4f59087c2a22265c0
SY-256 <yamamotosw1005@gmail.com>
2025-05-06 16:14:10 +0900

    5/6 commit

--- atcoder-introduction/chapter4-1/pattern90_055.py
+++ atcoder-introduction/chapter4-1/pattern90_055.py
@@ -1,10 +1,13 @@
-import itertools
 N, P, Q = map(int, input().split())
 A = list(map(int, input().split()))
-
 ans = 0
-comb = itertools.combinations(A, 5)
-for v in comb:
-    if (v[0] * v[1] * v[2] * v[3] * v[4]) % P == Q:
-        ans += 1
+count = 0
+for i1 in range(0, N):
+    for i2 in range(i1 + 1, N):
+        for i3 in range(i2 + 1, N):
+            for i4 in range(i3 + 1, N):
+                for i5 in range(i4 + 1, N):
+                    v = ((((A[i1] * A[i2] % P) * A[i3] % P) * A[i4] % P) * A[i5]) % P
+                    if v == Q:
+                        ans += 1
 print(ans)
