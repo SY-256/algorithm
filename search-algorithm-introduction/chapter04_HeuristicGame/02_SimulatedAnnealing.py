76b622d3a0d4719375e7f5d6be543092fb1a0ad0
SY-256 <yamamotosw1005@gmail.com>
2025-06-04 18:53:02 +0900

    6/4 commit

--- search-algorithm-introduction/chapter04_HeuristicGame/02_SimulatedAnnealing.py
+++ search-algorithm-introduction/chapter04_HeuristicGame/02_SimulatedAnnealing.py
@@ -1,14 +1,16 @@
 import random
+import math
 import copy
-from typing import Callable
 
-# 定数定義
+# 定数
 H = 5  # 迷路の高さ
 W = 5  # 迷路の幅
-END_TURN = 5  # ゲームの終了ターン
-CHARACTER_N = 3  # キャラクター数
-
+END_TURN = 5  # ゲーム終了ターン
+CHARACTER_N = 3  # キャラクターの数
 INF = 1e10
+
+# グローバル乱数生成器
+random.seed(0)
 
 
 class Coord:
@@ -20,34 +22,20 @@ class Coord:
 
 
 class AutoMoveMazeState:
-    """
-    自動一人ゲームの例
-    キャラクターは1マス先の最もポイントの高い床に自動で移動する。
-    合法手の中でスコアが同値のものがある場合、右、左、下、上の順で行動が優先される。
-    1ターンに上下左右四方向のいずれかに壁のない場所に1マスずつ進む。
-    床にあるポイントを踏むと自身のスコアとなり、床のポイントが消える。
-    END_TURNの時点のスコアを高くすることを目的とし、
-    ゲームに介入できる要素として、初期状態でのキャラクターをどこに配置するかを選択できる。
-    どのようにキャラクターを配置すると最終スコアが高くなるかを考えるゲーム。
-    """
+    """自動一人ゲームの状態を管理するクラス"""
 
-    # 方向ベクトル（右、左、下、上）
+    # 移動方向（右、左、下、上）の順
     dx = [1, -1, 0, 0]
     dy = [0, 0, 1, -1]
 
     def __init__(self, seed: int):
-        """h*wの迷路を生成する"""
         self.turn = 0
         self.game_score = 0
         self.evaluated_score = 0
-
-        # 床のポイントを1~9で表現する
         self.points = [[0 for _ in range(W)] for _ in range(H)]
-
-        # CHARACTER_N体のキャラクター
         self.characters = [Coord() for _ in range(CHARACTER_N)]
 
-        # シードを設定してポイントを生成
+        # 迷路の初期化
         random.seed(seed)
         for y in range(H):
             for x in range(W):
@@ -65,13 +53,11 @@ class AutoMoveMazeState:
         best_action_index = 0
 
         for action in range(4):
-            # actionの動きで上下左右の優先度を保ってランダムに探索
             ty = character.y + self.dy[action]
             tx = character.x + self.dx[action]
-
             if 0 <= ty < H and 0 <= tx < W:
                 point = self.points[ty][tx]
-                if point > best_point:
+                if best_point < point:
                     best_point = point
                     best_action_index = action
 
@@ -80,26 +66,24 @@ class AutoMoveMazeState:
 
     def advance(self):
         """ゲームを1ターン進める"""
-        # 全キャラクターを移動
-        for character_id in range(CHARACTER_N):
+        # キャラクターを移動
+        for character_id in CHARACTER_N:
             self.move_player(character_id)
 
-        # ポイントを取得してスコアに加算
+        # ポイント獲得
         for character in self.characters:
             point = self.points[character.y][character.x]
             self.game_score += point
-            self.points[character.y][character.x] = (
-                0  # 複数のキャラクターが同じマスに到達した場合は1回だけスコア加算
-            )
+            self.points[character.y][character.x] = 0
 
         self.turn += 1
 
-    def is_done(self) -> bool:
+    def is_done(self):
         """ゲームの終了判定"""
         return self.turn == END_TURN
 
-    def to_string(self) -> str:
-        """現在のゲーム状況を文字列にする"""
+    def to_string(self):
+        """現在のゲームの状況を文字列にする"""
         result = f"turn:\t{self.turn}\n"
         result += f"score:\t{self.game_score}\n"
 
@@ -123,9 +107,8 @@ class AutoMoveMazeState:
 
         return result
 
-    def get_score(self, is_print: bool = False) -> int:
+    def get_score(self, is_print: bool = False):
         """スコア計算をする"""
-        # 状態をコピー
         tmp_state = copy.deepcopy(self)
 
         # キャラクターの位置にあるポイントを消す
@@ -139,39 +122,3 @@ class AutoMoveMazeState:
                 print(tmp_state.to_string())
 
         return tmp_state.game_score
-
-
-def random_action(state: AutoMoveMazeState) -> AutoMoveMazeState:
-    """ランダムにキャラクターを配置する戦略"""
-    now_state = copy.deepcopy(state)
-
-    for character_id in range(CHARACTER_N):
-        y = random.randint(0, H - 1)
-        x = random.randint(0, W - 1)
-        now_state.set_character(character_id, y, x)
-
-    return now_state
-
-
-def play_game(
-    ai_name: str,
-    ai_function: Callable[[AutoMoveMazeState], AutoMoveMazeState],
-    seed: int,
-):
-    """ゲームを1回プレイしてゲイム状況を表示する"""
-    state = AutoMoveMazeState(seed)
-    state = ai_function(state)
-
-    print(state.to_string())
-    score = state.get_score(True)
-    print(f"Score of {ai_name}: {score}")
-
-
-def main():
-    # ランダムアクション戦略でプレイ
-    random.seed(0)  # アクション用のシードを設定
-    play_game("randomAction", random_action, 0)
-
-
-if __name__ == "__main__":
-    main()
