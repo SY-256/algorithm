7e5778c36da8429c07bee6a9e8b23edbb9cec9a6
SY-256 <yamamotosw1005@gmail.com>
2025-08-05 21:57:34 +0900

    commit 8/5

---
+++ atcoder-introduction/chapter4-4/abc068_c.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+g = [[] for _ in range(n)]
+
+for i in range(m):
+    a, b = map(int, input().split())
+
+    a -= 1
+    b -= 1
+
+    g[a].append(b)
+    g[b].append(a)
+
+for i in g:
+    if (n - 1) in i and 0 in i:
+        exit(print("POSSIBLE"))
+print("IMPOSSIBLE")
